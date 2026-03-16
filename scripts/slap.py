22e869b2681a7afa3b07246c66af371405769de4
Luke Brooks <luke.brooks42@gmail.com>
2026-03-16 10:00:43 +0000

    Slapping now echo'd to user in scripts/slap.py.

--- scripts/slap.py
+++ scripts/slap.py
@@ -1,4 +1,4 @@
-from scroll.script import on, irc
+from scroll.script import on, irc, echo
 
 
 @on("command:slap")
@@ -11,3 +11,4 @@ def cmd_slap(args):
         return
     irc.raw("PRIVMSG %s :\x01ACTION slaps %s around a bit with a large trout\x01"
             % (irc.current_channel, nick))
+    echo(irc.current_channel, "* %s slaps %s around a bit with a large trout" % (irc.nick, nick))
